9f983c098d9196936bf9fc768bde60b0155b6f57
Rainier <159208130+MtRainierUTD@users.noreply.github.com>
2025-04-20 03:26:57 -0500

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import BlobBackground from "./BlobBackground";
+import "./index.css";
+
+const App = () => {
+  return (
+    <>
+      <BlobBackground />
+      <div className="main-content">
+        <h1>Welcome to My Blob Site</h1>
+        <p>This is a test layout with floating animated blobs in the background.</p>
+      </div>
+    </>
+  );
+};
+
+export default App;
