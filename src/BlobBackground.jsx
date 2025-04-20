733852cb7e1d240dacdc67e5a39d11dd7ae7a676
Rainier <159208130+MtRainierUTD@users.noreply.github.com>
2025-04-20 03:26:32 -0500

    Create BlobBackground.jsx

---
+++ src/BlobBackground.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./BlobBackground.css";
+
+const BlobBackground = () => {
+  return (
+    <div className="blob-container">
+      <div className="blob blob1" />
+      <div className="blob blob2" />
+      <div className="blob blob3" />
+    </div>
+  );
+};
+
+export default BlobBackground;
